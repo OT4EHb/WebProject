6ef8a30eeacba3782f0958c9cb13f1915d4059be
StereoZing <94567076+StereoZing@users.noreply.github.com>
2024-12-05 04:06:57 +0300

    Add files via upload

--- js/Card.js
+++ js/Card.js
@@ -1,13 +1,14 @@
 ﻿let list;
 
 class Card {
-    constructor(name, opisanie, img, grammArr, priceArr, element) {
+    constructor(name, opisanie, img, grammArr, priceArr, animalsArr, element) {
         this.cartArr = new Array(grammArr.length).fill(0);
         this.name = name;
         this.opisanie = opisanie;
         this.img = img;
         this.grammArr = grammArr;
         this.priceArr = priceArr;
+        this.animalsArr = animalsArr;
         if (element === null) {
             let tovar = document.getElementById("tovar");
             let cardi = tovar.children[0].cloneNode(true);
