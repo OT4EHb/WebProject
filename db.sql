aa9b431dd5bc9e3598996bf30ef6918253f9c434
OT4EHb <guda1930@gmail.com>
2025-05-15 11:55:56 +0300

    card js to db

--- db.sql
+++ db.sql
@@ -1,7 +1,9 @@
+SET foreign_key_checks = 0;
 CREATE OR REPLACE TABLE cards(
 	card_id MEDIUMINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 	card_name VARCHAR(30) NOT NULL,
-	card_description TEXT NOT NULL
+	card_description TEXT NOT NULL,
+	card_img VARCHAR(30) NOT NULL
 ) ENGINE = INNODB;
 CREATE OR REPLACE TABLE animals(
 	animal_id SMALLINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
@@ -19,3 +21,11 @@ CREATE OR REPLACE TABLE card_animal(
 	FOREIGN KEY (animal_id) REFERENCES animals (animal_id),
 	PRIMARY KEY (card_id, animal_id)
 ) ENGINE = INNODB;
+CREATE OR REPLACE TABLE card_info(
+	card_id MEDIUMINT UNSIGNED,
+	card_gramm SMALLINT UNSIGNED,
+	card_price SMALLINT UNSIGNED NOT NULL,
+	FOREIGN KEY (card_id) REFERENCES cards (card_id),
+	PRIMARY KEY (card_id,card_gramm)
+) ENGINE = INNODB;
+SET foreign_key_checks = 1;
